361c95ddb42050a0388794d247eb5b18153c065f
Thalyson Lima da Silva <devthalyson72@gmail.com>
2024-05-08 18:28:10 -0300

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function calcular(){
     let num = window.document.querySelector('input#txtnum')
     let tab = window.document.getElementById('seltab')
+    let limp = window.document.getElementById('clean')
+    limp.addEventListener('click', limpar)
     if(num.value == 0){
         alert('[ERRO!] Informe um número!')
     } else {
@@ -11,9 +13,12 @@ function calcular(){
             item.text = `${n}x${i}=${n*i}`
             item.value = `${n}`
             tab.appendChild(item)
-            
-
-            
         }
-    }
+   }
+   function limpar(){
+    tab.innerHTML = ``
+    
+    
 }
+}
+
