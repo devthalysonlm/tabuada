745937437d6c41a63de4371f9f7a3098a3963676
Thalyson Lima da Silva <devthalyson72@gmail.com>
2024-05-08 16:49:47 -0300

    index.js
    Inserindo lógica JavaScript

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Olá,Mundo!')
+function calcular(){
+    let num = window.document.querySelector('input#txtnum')
+    let tab = window.document.getElementById('seltab')
+    if(num.value == 0){
+        alert('[ERRO!] Informe um número!')
+    } else {
+        tab.innerHTML = ''
+        let n = Number(num.value)
+        for (let i = 1; i <= 10; i++) {
+            let item = document.createElement('option')
+            item.text = `${n}x${i}=${n*i}`
+            item.value = `${n}`
+            tab.appendChild(item)
+            
+
+            
+        }
+    }
+}
